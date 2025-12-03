30d7aa19548ad86dfacce2939724ed723b90231a
dadic <mwisenezadadic2000@gmail.com>
2025-12-03 19:20:12 +0200

    Index page is created.

---
+++ dadic_MWISENEZA/src/index.php
@@ -0,0 +1,19 @@
+<?php
+// index.php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>HEALTHRWANDA – Hospital Management Portal</title>
+</head>
+<body>
+    <h1>Welcome to HEALTHRWANDA System</h1>
+
+    <p>Please choose an option:</p>
+    <ul>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="login.php">Login</a></li>
+    </ul>
+</body>
+</html>
